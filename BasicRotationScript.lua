c29bfebad5dac845ec1a941a9fab47e8769d1a80
Matthew <115804086+MensonRbx@users.noreply.github.com>
2023-06-15 23:22:31 +0100

    Update BasicRotationScript.lua
    Disabled some goofy rotation stuff, making the waist rotation smoother.

--- BasicRotationScript.lua
+++ BasicRotationScript.lua
@@ -7,35 +7,30 @@ local RunService = game:GetService('RunService')
 local currentCamera = workspace.CurrentCamera
 local character = script.Parent
 local humanoidRootPart = character:WaitForChild('HumanoidRootPart')
-local neckMotor6D = character:FindFirstChild('Neck', true)
-local waistMotor6D  = character:FindFirstChild('Waist', true)
+local neckMotor6D = character:WaitForChild("Head"):WaitForChild("Neck")
+local waistMotor6D  = character:WaitForChild("UpperTorso"):WaitForChild("Waist")
 
 --Get this first so it remains constant, no movement of head up/down
 local neck_Y = neckMotor6D.C0.Y
 local waist_Y = waistMotor6D.C0.Y
 
 --constants
-local CFRAME_NEW = CFrame.new
-local CFRAME_ANGLES = CFrame.Angles
-local MATH_ASIN = math.asin
-local MATH_ABS = math.abs
+local CFRAME_NEW, CFRAME_ANGLES, MATH_ASIN, MATH_ABS = CFrame.new, CFrame.Angles, math.asin, math.abs
 
 RunService.RenderStepped:Connect(function()
 	local relative_camera_position: CFrame = humanoidRootPart.CFrame:ToObjectSpace(currentCamera.CFrame)
 	local direction_of_camera: Vector3 = relative_camera_position.LookVector
 	
 	local absolute_x_rotation = MATH_ABS(direction_of_camera.X)
+	local max_neck_rotation = 0.7
 	
-	local right = direction_of_camera.X > 0.5
-	local left = direction_of_camera.X < -0.5 
+	local is_looking_left = direction_of_camera.X < - max_neck_rotation
 	
-	local clamp = 0.5
-	
-	if absolute_x_rotation > 0.5 then 
-		if left then
-			clamp *= -1
+	if absolute_x_rotation > max_neck_rotation then 
+		if is_looking_left then
+			max_neck_rotation *= -1
 		end
-		waistMotor6D.C0 = CFRAME_NEW(0, waist_Y, 0) * CFRAME_ANGLES(0,(-MATH_ASIN(direction_of_camera.X) + clamp),0) * CFRAME_ANGLES(MATH_ASIN(direction_of_camera.Y),0,0)
+		waistMotor6D.C0 = CFRAME_NEW(0, waist_Y, 0) * CFRAME_ANGLES(0,(-MATH_ASIN(direction_of_camera.X) + max_neck_rotation),0)
 	else
 		waistMotor6D.C0 = CFRAME_NEW(0, waist_Y, 0)
 		neckMotor6D.C0 = CFRAME_NEW(0,neck_Y,0) * CFRAME_ANGLES(0,-MATH_ASIN(direction_of_camera.X),0) * CFRAME_ANGLES(MATH_ASIN(direction_of_camera.Y),0,0)
